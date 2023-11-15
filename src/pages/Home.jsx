289af13c67484baab558ab7be6ae97456ae11da4
John Assefa <johnassefaforbussines@gmail.com>
2023-11-14 16:35:52 -0800

    setting pages

---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Home=()=>{
+    return(
+0
+    )
+}
+
+export  default Home;
---
+++ src/pages/Projects.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Projects=()=>{
+    return(
+0
+    )
+}
+
+export  default Projects;
