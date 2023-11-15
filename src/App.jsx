07534bb11d720c9de332ef08a31f779d411d519a
John Assefa <johnassefaforbussines@gmail.com>
2023-11-14 16:04:19 -0800

    setting navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,23 @@
+import { Route, BrowserRouter as Router , Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+
 
 function App() {
   return (
-  <div className="underline">working</div>  
-  )
+<main>
+
+    <Router>
+      <Navbar></Navbar>
+      <Routes>
+        <Route path="/" element={'home'}/>
+        <Route path="/about" element={'about'}/>
+        <Route path="/project" element={'project'}/>
+        <Route path="/contact " element={'contact'}/>
+
+      </Routes>
+    </Router>
+
+</main>  )
 }
 
 export default App;
