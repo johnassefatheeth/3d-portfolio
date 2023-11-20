9380c281864afa300a67420c9eff194a1f420441
John Assefa <johnassefaforbussines@gmail.com>
2023-11-20 03:39:40 -0800

    editing the hoe page

--- src/components/HomeComponent.jsx
+++ src/components/HomeComponent.jsx
@@ -1,37 +1,65 @@
-import React from "react";
+import { Link } from "react-router-dom";
 
+import { arrow } from "../assets/icons";
 
-
-const renderContent={
-    1:(
-        <h1 className='sm:text-xl sm:leading-snug text-center neo-brutalism-blue py-4 px-8 text-white mx-5'>
+const HomeComponent = ({ currentstage }) => {
+  if (currentstage === 1)
+    return (
+      <h1 className='sm:text-xl sm:leading-snug text-center neo-brutalism-blue py-4 px-8 text-white mx-5'>
         Hi, I'm
         <span className='font-semibold mx-2 text-white'>John</span>
         👋
         <br />
         A Software Engineer from Ethiopia
       </h1>
-    ),
-    2:(
-        <h1>2</h1>
-    ),
-    3:(
-        <h1>3</h1>
-    ),
-    4:(
-        <h1>4</h1>
-    )
-}
+    );
 
-const infoBox =({text,link,btntext})=>{
-    <div className="info-box">
-        {text}
+  if (currentstage === 2) {
+    return (
+      <div className='info-box'>
+        <p className='font-medium sm:text-xl text-center'>
+          Highly Entusiastic <br /> and Energetic young lad
+        </p>
+
+        <Link to='/about' className='neo-brutalism-white neo-btn'>
+          Learn more
+          <img src={arrow} alt='arrow' className='w-4 h-4 object-contain' />
+        </Link>
+      </div>
+    );
+  }
+
+  if (currentstage === 3) {
+    return (
+      <div className='info-box'>
+        <p className='font-medium text-center sm:text-xl'>
+          made several projects <br /> wana check them out
+        </p>
+
+        <Link to='/projects' className='neo-brutalism-white neo-btn'>
+          Visit my portfolio
+          <img src={arrow} alt='arrow' className='w-4 h-4 object-contain' />
+        </Link>
+      </div>
+    );
+  }
+
+  if (currentstage === 4) {
+    return (
+      <div className='info-box'>
+      <p className='font-medium sm:text-xl text-center'>
+        are you interested in talking to me <br/> me too!!!
+      </p>
+
+      <Link to='/contact' className='neo-brutalism-white neo-btn'>
+        Let's talk
+        <img src={arrow} alt='arrow' className='w-4 h-4 object-contain' />
+      </Link>
     </div>
-}
+    );
+  }
 
-const HomeComponent =({currentstage})=>{
-    return renderContent[currentstage] || null;
-    
-}
+  return null;
+};
 
 export default HomeComponent;
