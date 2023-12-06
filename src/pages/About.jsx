73f4af46680e8446891a05cf238dd998e0292156
John Assefa <johnassefaforbussines@gmail.com>
2023-12-06 12:25:26 -0800

    started the about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,7 +2,35 @@ import React from "react";
 
 const About=()=>{
     return(
-0
+        <section className="max-container">
+            <h1 className="head-text">
+                HI there ,this is<span className="blue-gradient_text font-semibold drop-shadow">John</span>
+            </h1>
+
+            <div className="mt-s flex flex-col gap-3 text-slate-500">
+                <p>
+                a software engineer with advanced front-end
+                 and moderate
+                 back-end skills. I am also good at UI/UX
+                 design and have a natural inclination for 
+                 digital art. Constantly seeking out new 
+                 challenges and opportunities to expand my 
+                 knowledge and skills, I am passionate about 
+                 software development and eager to make a 
+                 significant contribution to the field. 
+                  With a combination of 
+                  technical expertise and a creative flair,
+                   I am poised to achieve great 
+                things in the field of software engineering.
+                </p>
+            </div>
+            <div className="py-10 flex flex-col">
+                <h3 className="subheaded-text">My Skills</h3>
+                <div className="mt-16 flex flex-warp gap-12">
+                    
+                </div>
+            </div>
+        </section>
     )
 }
 
